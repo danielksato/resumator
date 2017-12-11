236687fec34268bbcec58ee3aef08d4115caaa9e
Daniel Sato <dksato@gmail.com>
2017-12-11 17:48:15 -0600

    configuration

--- src/components/aboutPage.js
+++ src/components/aboutPage.js
@@ -1,30 +1,34 @@
 import React, { Component, PropTypes as PT } from 'react';
 
 export default class AboutPage extends Component {
-	renderPhoto() {
-		return this.props.image ? (
-			<div className="headshot-container">
-				<img className="headshot" src={this.props.image} />
-			</div>
-		) : null;
-	}
+  renderPhoto() {
+    return this.props.image ? (
+      <div className="headshot-container">
+        <img className="headshot" src={this.props.image} />
+      </div>
+    ) : null;
+  }
 
-	renderParagraphs() {
-		return (
-			<div>
-				{this.props.about.map((text, index) => (
-					<p key={`about-${index}`}>{text}</p>
-				))}
-			</div>
-		);
-	}
+  renderParagraphs() {
+    return <div>{this.props.about.map((text, index) => <p key={`about-${index}`}>{text}</p>)}</div>;
+  }
 
-	render() {
-		return (
-			<div>
-				{this.renderPhoto()}
-				{this.renderParagraphs()}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {this.renderPhoto()}
+        {this.renderParagraphs()}
+      </div>
+    );
+  }
 }
+
+AboutPage.propTypes = {
+  image: PT.string.isRequired(),
+  about: PT.arrayOf(PT.string).isRequired(),
+};
+
+AboutPage.defaultProps = {
+  image: '',
+  about: [''],
+};
